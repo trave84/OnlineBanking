232ba709aa6065382b8a0d19ee51dc3cfd5de46e
trave84 <markerterheaven@gmail.com>
2018-12-21 16:37:11 +0100

    Example static client renders in UI with soe details

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -1,12 +1,63 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Clients extends Component {
   render() {
-    return (
-      <div>
-        <h1>Clients</h1>
-      </div>
-    );
+    const clients = [
+      {
+        id: "347814",
+        firstName: "Kev",
+        lastName: "Gmes",
+        email: "a@gmailcom",
+        phone: "3434 4323",
+        balance: "34500"
+      }
+    ];
+
+    if (clients) {
+      return (
+        <div className="row">
+          <div className="col-md-6 ">
+            <h2>
+              {" "}
+              <i className="fas fa-users" /> Clients
+            </h2>
+          </div>
+          <div className="col-md-6" />
+          <table className="table">
+            <thead className="inverse">
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Balance</th>
+              </tr>
+            </thead>
+            <tbody>
+              {clients.map(client => (
+                // KEY ot ID
+                <tr key={client.id}>
+                  <td>
+                    {client.firstName} {client.lastName}
+                  </td>
+                  <td>{client.email}</td>
+                  <td>{client.balance}</td>
+                  <td>
+                    <Link
+                      to={`/client/${client.id}`}
+                      className="btn btn-secondary btn-sm"
+                    >
+                      <i className="fas fa-arrow-circle-right"> Details</i>
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      );
+    } else {
+      return <h1>Loading..</h1>;
+    }
   }
 }
 export default Clients;
