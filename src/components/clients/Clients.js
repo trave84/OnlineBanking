19e2d68b6283fb9da959a270e395428a0e898620
trave84 <markerterheaven@gmail.com>
2018-12-21 16:02:31 +0100

    Basic UI for clients pge + Navbar set up.

---
+++ src/components/clients/Clients.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Clients extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Clients</h1>
+      </div>
+    );
+  }
+}
+export default Clients;
